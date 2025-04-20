662006e3b47b4b6c18742bbef8861c3dae794c9b
Narendra Singh <narendrasinghsitu4@gmail.com>
2025-04-20 22:40:37 +0530

    backend deploty and api connect to frontend

--- startBoxFrontend/src/features/api/apiSlice.js
+++ startBoxFrontend/src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react' ;
 
 export const apiSlice =createApi({
     reducerPath:"api",
-    baseQuery:fetchBaseQuery({baseUrl:"http://localhost:8080/"}),
+    baseQuery:fetchBaseQuery({baseUrl:"https://startbox-habt.onrender.com/"}),
     endpoints:(builder)=>({
 
         // for register user
